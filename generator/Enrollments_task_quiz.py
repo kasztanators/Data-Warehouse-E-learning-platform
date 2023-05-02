5051627698fc044ecb182cc99cccad4ba93dbeab
Wiktor Czetyrbok <88346002+kasztanators@users.noreply.github.com>
2023-05-02 13:06:49 +0200

    Enrollments  hour minute seconds

--- generator/Enrollments_task_quiz.py
+++ generator/Enrollments_task_quiz.py
@@ -14,8 +14,8 @@ fieldnames_quiz_res = ["ID", "Date of submission", "Grade", "Quiz_ID", "Enrollme
 
 # Generate 500000 rows of data
 enrollments_rows = 500000
-start_date = datetime(2015, 1, 1)
-end_date = datetime(2016, 12, 31)
+start_date = datetime(2015, 1, 1, 0, 0, 0)
+end_date = datetime(2016, 12, 31, 23, 59, 59)
 with open(filename_enrol, "w", newline="") as csvfile,open(task_res, "w", newline="") as task_csv,open(filename_quiz_res, "w", newline="") as quiz_csv:
     writer_enr = csv.DictWriter(csvfile, fieldnames=fieldnames_enrol)
     writer_enr.writeheader()
@@ -27,10 +27,10 @@ with open(filename_enrol, "w", newline="") as csvfile,open(task_res, "w", newlin
     for i in range(1, enrollments_rows+1):
         sum = 0
         if i > 250000:
-            start_date = datetime(2016, 1, 1)
-            end_date = datetime(2017, 12, 31)
+            start_date = datetime(2016, 1, 1, 0, 0, 0)
+            end_date = datetime(2017, 12, 31, 23, 59, 59)
         # Generate random data for each row
-        date_start = start_date + timedelta(days=random.randint(0, 365))
+        date_start = start_date + timedelta(days=random.randint(0, 365), hours=random.randint(0, 23), minutes=random.randint(0, 59), seconds=random.randint(0, 59))
         grade = random.randint(2, 5)
 
         percent_complete = random.randint(0, 100)
@@ -41,13 +41,13 @@ with open(filename_enrol, "w", newline="") as csvfile,open(task_res, "w", newlin
         course_id = random.randint(1, 1000)
 
         # Write the row to the CSV file
-        date_complete = date_start + timedelta(days=random.randint(103, 200))
+        date_complete = date_start + timedelta(days=random.randint(103, 200), hours=random.randint(0, 23), minutes=random.randint(0, 59), seconds=random.randint(0, 59))
         for j in range(1, 5):
             grade = random.randint(2, 5)
             sum+=grade
-            ass_submission_date = date_start + timedelta(days=random.randint(0, 100))
+            ass_submission_date = date_start + timedelta(days=random.randint(0, 100), hours=random.randint(0, 23), minutes=random.randint(0, 59), seconds=random.randint(0, 59))
             while True:
-                ass_submission_date = date_start + timedelta(days=random.randint(0, 100))
+                ass_submission_date = date_start + timedelta(days=random.randint(0, 100), hours=random.randint(0, 23), minutes=random.randint(0, 59), seconds=random.randint(0, 59))
                 if ass_submission_date < date_complete:
                     break
             assigment_id = ((i - 1) * 4) + j
@@ -81,10 +81,10 @@ with open(filename_enrol, "w", newline="") as csvfile,open(task_res, "w", newlin
             date_complete =None
         writer_enr.writerow({
             "ID": i,
-            "Date of start": date_start.strftime("%Y-%m-%d"),
+            "Date of start": date_start.strftime("%Y-%m-%d %H:%M:%S"),
             "Grade": round((sum / 8) * 2) / 2,
             "Percentage of completion": percent_complete,
-            "Completion date": date_complete.strftime("%Y-%m-%d")if date_complete else "",
+            "Completion date": date_complete.strftime("%Y-%m-%d %H:%M:%S") if date_complete else "",
             "StudentID": student_id,
             "CourseID": course_id
         })
