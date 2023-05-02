a4542967fe682de04a7fa075b18cd3e3d7dd9dcb
Wiktor Czetyrbok <88346002+kasztanators@users.noreply.github.com>
2023-05-02 10:19:26 +0200

    Order changed

--- ETL/ETL_load_uknown.sql
+++ ETL/ETL_load_uknown.sql
@@ -65,14 +65,14 @@ INSERT INTO Dim_Survey (ID_Survey, Status) VALUES (1, 'fulfilled');
 INSERT INTO Dim_Survey (ID_Survey, Status) VALUES (2, 'not fulfilled');
 SET IDENTITY_INSERT dbo.Dim_Survey OFF; 
 
-SET IDENTITY_INSERT dbo.Dim_Student ON; 
-INSERT INTO Dim_Student (ID_Student, LastName, FirstName, Email,IsCurrent,StartTime, EndTime) VALUES (-1, 'unknown', 'unknown', 'unknown',NULL, NULL, NULL);
-SET IDENTITY_INSERT dbo.Dim_Student OFF; 
-
+--- execute after loading all dimensions
 SET IDENTITY_INSERT dbo.Dim_Date ON; 
 INSERT INTO Dim_Date (ID_Date, Date, Year, Month, MonthNo, DayOfWeek, DayOfWeekNo) VALUES (-1, null, null, 'unknown', NULL, 'unknown', NULL);
 SET IDENTITY_INSERT dbo.Dim_Date OFF; 
 
+SET IDENTITY_INSERT dbo.Dim_Student ON; 
+INSERT INTO Dim_Student (ID_Student, LastName, FirstName, Email,IsCurrent,StartTime, EndTime) VALUES (-1, 'unknown', 'unknown', 'unknown',NULL, NULL, NULL);
+SET IDENTITY_INSERT dbo.Dim_Student OFF; 
 
 SET IDENTITY_INSERT dbo.Dim_Tutor ON; 
 INSERT INTO Dim_Tutor (ID_Tutor, FirstName, LastName) VALUES (-1, 'unknown', 'unknown');
