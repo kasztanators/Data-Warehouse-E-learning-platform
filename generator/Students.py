bc7dd462a50221cbea637c92794ddaecd8bfbaee
Wiktor Czetyrbok <88346002+kasztanators@users.noreply.github.com>
2023-05-12 07:50:21 +0200

    field addedd

--- generator/Students.py
+++ generator/Students.py
@@ -10,11 +10,11 @@ for i in range(1, num_rows+1):
     name = fake.first_name()
     surname = fake.last_name()
     email = fake.email()
-    data.append([i, name, surname, email])
+    data.append([i, name, surname, email, i])
 
 # Write the data to a CSV file
 filename = "Students.csv"
 with open(filename, "w", newline="") as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow(["ID", "Name", "Surname", "Email"])
+    writer.writerow(["ID", "Name", "Surname", "Email","Index"])
     writer.writerows(data)
